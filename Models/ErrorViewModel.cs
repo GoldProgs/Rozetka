f9907ffdc9401cbcf21e2d21812956518ff90285
Kostya <emelyanov4512@gmail.com>
2024-05-15 19:59:06 +0300

    Создал проект

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RozetkaProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
